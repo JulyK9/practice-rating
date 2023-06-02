590cd94b710e3540d9ed1f49b5ef3230604bd0e8
JKKIM <jkesonjk@gmail.com>
2023-06-03 00:10:35 +0900

    rating 증가시키는 set함수 추가 및 onClick 등록

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -2,18 +2,20 @@ import { StarIcon } from '@heroicons/react/20/solid';
 
 interface StarProps {
   selected: boolean;
+  onSelect: () => void;
 }
 
-const Star: React.FC<StarProps> = ({ selected = false }) => {
-  console.log('selected: ', selected);
+const Star: React.FC<StarProps> = ({ selected = false, onSelect }) => {
+  // console.log('selected: ', selected);
   return (
     <div
       className={`
       w-[50px]
-      ${selected ? 'text-zinc-500' : 'text-red-500'}
+      cursor-pointer
+      ${selected ? 'text-red-500' : 'text-zinc-500'}
       `}
     >
-      <StarIcon />
+      <StarIcon onClick={onSelect} />
     </div>
   );
 };
--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -8,13 +8,18 @@ interface StarRatingProps {
 const createStarArray = (length: number) => [...Array(length)];
 
 const StarRating: React.FC<StarRatingProps> = ({ totalStars }) => {
-  const [selectedStars] = useState(3);
+  const [selectedStars, setSelectedStars] = useState(0);
 
   return (
     <div className="flex flex-col justify-center items-center mt-5">
       <div className="flex">
-        {createStarArray(totalStars).map((n, i) => (
-          <Star key={i} selected={selectedStars > i} />
+        {createStarArray(totalStars).map((_n, i) => (
+          <Star
+            key={i}
+            selected={selectedStars > i}
+            onSelect={() => setSelectedStars(i + 1)}
+            // onSelect={(prev: number) => setSelectedStars(prev + 1)}
+          />
         ))}
       </div>
       <p>
