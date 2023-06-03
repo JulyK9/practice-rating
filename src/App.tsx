d15a018e17b4896f3d0ff369e36e811a6637a626
jk929jk <jkesonjk@gmail.com>
2023-06-03 06:00:09 +0000

    재사용성을 고려한 배경 스타일 추가 로직 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
-import StarRating from './components/StarRating';
+import "./App.css";
+import StarRating from "./components/StarRating";
 
 function App() {
   return (
     <div className="w-full h-full bg-slate-100">
-      <StarRating totalStars={5} />
+      <StarRating totalStars={5} bgColor="yellow" />
+      <StarRating totalStars={5} bgColor="sky" />
+      <StarRating totalStars={5} bgColor="teal" />
     </div>
   );
 }
--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -1,17 +1,21 @@
-import { useState } from 'react';
-import Star from './Star';
+import { useState } from "react";
+import Star from "./Star";
 
 interface StarRatingProps {
   totalStars: number;
+  bgColor: string;
 }
 
 const createStarArray = (length: number) => [...Array(length)];
 
-const StarRating: React.FC<StarRatingProps> = ({ totalStars }) => {
+const StarRating: React.FC<StarRatingProps> = ({ totalStars, ...props }) => {
   const [selectedStars, setSelectedStars] = useState(0);
-
+  const { bgColor } = props;
+  // console.log(bgColor);
   return (
-    <div className="flex flex-col justify-center items-center mt-5">
+    <div
+      className={`bg-${bgColor}-200 flex flex-col justify-center items-center p-5 gap-2`}
+    >
       <div className="flex">
         {createStarArray(totalStars).map((_n, i) => (
           <Star
